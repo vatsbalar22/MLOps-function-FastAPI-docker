89d7befad3014e080a7fc6fc8167b5baeb1d5a38
Vats Balar <141894006+vatsbalar22@users.noreply.github.com>
2024-02-07 06:21:23 +0000

    fix issue

--- fire-cli.py
+++ fire-cli.py
@@ -1,7 +1,7 @@
 import fire
 from mylib.bot import scrape
 
-if __name__ == '__main__':
-  fire.Fire(scrape)
+if __name__ == "__main__":
+    fire.Fire(scrape)
 
 #   python fire-cli.py --name Microsoft --length 1
