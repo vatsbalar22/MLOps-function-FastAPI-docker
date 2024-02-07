dd5511c14e9ea4f44af2565460acd872e8500bd7
Vats Balar <141894006+vatsbalar22@users.noreply.github.com>
2024-02-07 05:27:07 +0000

    adding files to repo

--- test_wikibot.py
+++ test_wikibot.py
@@ -2,11 +2,13 @@ from mylib.bot import scrape
 from click.testing import CliRunner
 from wikibot import cli
 
+
 def test_scrape():
-    assert 'Microsoft' in scrape('Microsoft')
+    assert "Microsoft" in scrape("Microsoft")
+
 
 def test_wikibot():
-  runner = CliRunner()
-  result = runner.invoke(cli,['--name','Microsoft'])
-  assert result.exit_code == 0
-  assert 'Microsoft' in result.output
+    runner = CliRunner()
+    result = runner.invoke(cli, ["--name", "Microsoft"])
+    assert result.exit_code == 0
+    assert "Microsoft" in result.output
--- wikibot.py
+++ wikibot.py
@@ -1,15 +1,13 @@
 from mylib.bot import scrape
 import click
 
-@click.command()
-@click.option('--name',help='webpage want to scrape')
 
+@click.command()
+@click.option("--name", help="webpage want to scrape")
 def cli(name):
     result = scrape(name)
-    click.echo(click.style(f'{result} :' ,bg='yellow', fg='black'))
+    click.echo(click.style(f"{result} :", bg="yellow", fg="black"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
-
-
-    
